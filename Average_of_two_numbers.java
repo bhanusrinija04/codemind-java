61c1699bde98617d1e5b5a03b8838c4ba98db16d
bhanusrinija04 <143777671+bhanusrinija04@users.noreply.github.com>
2023-09-01 10:49:55 +0530

    import java.util.Scanner; public class average {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         double c=(a+b)*0.5;         System.out.printf("Average of "+a+" and "+ b+" is: %.2f",c);     } }

---
+++ Average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class average
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double c=(a+b)*0.5;
+        System.out.printf("Average of "+a+" and "+ b+" is: %.2f",c);
+    }
+}
