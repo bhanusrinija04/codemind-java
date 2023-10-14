ae0c99f6e1849345fdb47a8675cc2a8c5a45b23b
bhanusrinija04 <143777671+bhanusrinija04@users.noreply.github.com>
2023-10-14 10:11:49 +0530

    import java.util.*; public class Pattern11 {
        public static void main(String[] args)
        {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=1;i<=n;i++)
         {
             for(int j=1;j<=n;j++)
             {
                 System.out.print((char)(64+i)+" ");
             }
             System.out.println();
         }
        }
    }

---
+++ print_the_following_pattern_11.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Pattern11
+{
+
+    public static void main(String[] args)
+    {
+     Scanner sc=new Scanner(System.in);
+     int n=sc.nextInt();
+     for(int i=1;i<=n;i++)
+     {
+         for(int j=1;j<=n;j++)
+         {
+             System.out.print((char)(64+i)+" ");
+         }
+         System.out.println();
+     }
+    }
+}
