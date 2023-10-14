f05cb888cbcddc5c08be96145e270f4be409e923
bhanusrinija04 <143777671+bhanusrinija04@users.noreply.github.com>
2023-10-14 11:21:38 +0530

    import java.util.*; public class Cars {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         //System.out.println(n%4==0?n/4:(n/4)+1);         if(n%4==0)         {             System.out.println(n/4);         }         else         {             System.out.println((n/4)+1);         }     } }

--- Cars.java
+++ Cars.java
@@ -5,6 +5,14 @@ public class Cars
     {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        System.out.println(n%4==0?n/4:(n/4)+1);
+        //System.out.println(n%4==0?n/4:(n/4)+1);
+        if(n%4==0)
+        {
+            System.out.println(n/4);
+        }
+        else
+        {
+            System.out.println((n/4)+1);
+        }
     }
 }
