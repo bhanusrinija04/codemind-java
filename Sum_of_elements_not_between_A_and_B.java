71953e940c4499667de89c1728ae22445755e164
bhanusrinija04 <143777671+bhanusrinija04@users.noreply.github.com>
2023-10-20 12:04:13 +0530

    import java.util.*; public class Elements{     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         int a=sc.nextInt();         int b=sc.nextInt();         int Sum=0;         for(int i=0;i<n;i++)         {             if(arr[i]<=b&&arr[i]>=a)             {                 continue;             }             else{             Sum+=arr[i];             }         }         System.out.println(Sum);     } }

---
+++ Sum_of_elements_not_between_A_and_B.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Elements{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int Sum=0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]<=b&&arr[i]>=a)
+            {
+                continue;
+            }
+            else{
+            Sum+=arr[i];
+            }
+        }
+        System.out.println(Sum);
+    }
+}
